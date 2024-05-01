43eeb2ad25e4f0efa093aff50e519095067178ed
Ali Abdullah <aliila-3@hotmail.com>
2024-05-01 21:27:18 +0200

    Test

--- src/main/java/com/example/pensionatdb/models/Kund.java
+++ src/main/java/com/example/pensionatdb/models/Kund.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.antlr.v4.runtime.misc.NotNull;
 
@@ -18,6 +21,9 @@ public class Kund {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotEmpty(message = "Name is Mandatory")
+    @Size(min = 3, message ="At least 3 Letters ")
+    @Pattern(regexp="^[A-Öa-ö]*$", message = "Only Letter for Name")
     @NotNull
     private String namn;
     @NotNull
