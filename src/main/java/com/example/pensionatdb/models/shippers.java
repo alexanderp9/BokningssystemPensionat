e52da311c7ab23b86988ac5590bced5020c18365
alexanderp9 <alexander.pino@hotmail.com>
2024-05-09 12:24:16 +0200

    städat lite i koden

---
+++ src/main/java/com/example/pensionatdb/models/shippers.java
@@ -0,0 +1,15 @@
+package com.example.pensionatdb.models;
+
+public class shippers{
+    public int id;
+    public String email;
+    public String companyName;
+    public String contactName;
+    public String contactTitle;
+    public String streetAddress;
+    public String city;
+    public String postalCode;
+    public String country;
+    public String phone;
+    public String fax;
+}
