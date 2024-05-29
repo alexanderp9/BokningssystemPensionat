beba52f8b46843555d7243a90ead0775a9d40415
Ali Abdullah <aliila-3@hotmail.com>
2024-05-29 16:50:28 +0200

    FixTEST

--- src/test/java/com/example/pensionatdb/Tests/BokningServiceTest.java
+++ src/test/java/com/example/pensionatdb/Tests/BokningServiceTest.java
@@ -3,10 +3,12 @@ import com.example.pensionatdb.dtos.BokningDTO;
 import com.example.pensionatdb.models.Bokning;
 import com.example.pensionatdb.models.Kund;
 
+import com.example.pensionatdb.models.Rum;
 import com.example.pensionatdb.repos.bokningRepo;
 import com.example.pensionatdb.repos.kundRepo;
 import com.example.pensionatdb.repos.rumRepo;
 import com.example.pensionatdb.services.BokningService;
+import com.example.pensionatdb.services.DiscountService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -14,11 +16,14 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 public class BokningServiceTest {
@@ -32,6 +37,9 @@ public class BokningServiceTest {
     @Mock
     private rumRepo mockRumRepo;
 
+    @Mock
+    private DiscountService mockDiscountService;  // Add this line
+
     @InjectMocks
     private BokningService bokningService;
 
@@ -39,6 +47,7 @@ public class BokningServiceTest {
     public void setup() {
         MockitoAnnotations.initMocks(this);
     }
+
 
     @Test
     public void testFindBokningarByKund() {
@@ -50,38 +59,38 @@ public class BokningServiceTest {
         assertEquals(2, result.size());
         verify(mockBokningRepo, times(1)).findByKund(kund);
     }
+    @Test
+    public void testConvertToEntity() {
 
-    //    @Test
-//    public void testConvertToEntity() {
-//
-//        BokningDTO bokningDTO = new BokningDTO();
-//        bokningDTO.setId(1L);
-//        bokningDTO.setNätter(3);
-//        bokningDTO.setStartSlutDatum("20240101-20240104");
-//        bokningDTO.setKundId(1L);
-//        bokningDTO.setRumId(1L);
-//        bokningDTO.setAvbokad(false);
-//
-//
-//        Kund mockKund = new Kund();
-//        mockKund.setId(1L);
-//        when(mockKundRepo.findById(1L)).thenReturn(Optional.of(mockKund));
-//
-//        Rum mockRum = new Rum();
-//        mockRum.setId(1L);
-//        when(mockRumRepo.findById(1L)).thenReturn(Optional.of(mockRum));
-//
-//
-//        Bokning result = bokningService.convertToEntity(bokningDTO);
-//
-//
-//        assertEquals(bokningDTO.getId(), result.getId());
-//        assertEquals(bokningDTO.getNätter(), result.getNätter());
-//        assertEquals(bokningDTO.getStartSlutDatum(), bokningDTO.getStartSlutDatum());
-//        assertEquals(mockKund, result.getKund());
-//        assertEquals(mockRum, result.getRum());
-//        assertEquals(bokningDTO.isAvbokad(), result.isAvbokad());
-//    }
+        BokningDTO bokningDTO = new BokningDTO();
+        bokningDTO.setId(1L);
+        bokningDTO.setNätter(3);
+        bokningDTO.setStartDatum(LocalDate.parse("2024-01-01"));
+        bokningDTO.setSlutDatum(LocalDate.parse("2024-01-04"));
+        bokningDTO.setKundId(1L);
+        bokningDTO.setRumId(1L);
+        bokningDTO.setAvbokad(false);
+
+
+        Kund mockKund = new Kund();
+        mockKund.setId(1L);
+        when(mockKundRepo.findById(1L)).thenReturn(Optional.of(mockKund));
+
+        Rum mockRum = new Rum();
+        mockRum.setId(1L);
+        when(mockRumRepo.findById(1L)).thenReturn(Optional.of(mockRum));
+
+   Bokning result = bokningService.convertToEntity(bokningDTO);
+
+
+       assertEquals(bokningDTO.getId(), result.getId());
+       assertEquals(bokningDTO.getNätter(), result.getNätter());
+       assertEquals(bokningDTO.getStartDatum(), result.getStartDatum());
+       assertEquals(bokningDTO.getSlutDatum(), result.getSlutDatum());
+       assertEquals(mockKund, result.getKund());
+       assertEquals(mockRum, result.getRum());
+       assertEquals(bokningDTO.isAvbokad(), result.isAvbokad());
+    }
     @Test
     public void testGetAllBokningDTOs() {
 
@@ -113,52 +122,55 @@ public class BokningServiceTest {
         verify(mockBokningRepo, times(1)).deleteById(bokningId);
     }
 
-//    @Test
-//    public void testAddBokningFromDTO() {
-//
-//        BokningDTO bokningDTO = new BokningDTO();
-//        bokningDTO.setId(1L);
-//        bokningDTO.setNätter(3);
-//        bokningDTO.setStartSlutDatum("240101-240104");
-//        bokningDTO.setKundId(1L);
-//        bokningDTO.setRumId(1L);
-//        bokningDTO.setAvbokad(false);
-//
-//
-//        Kund mockKund = new Kund();
-//        mockKund.setId(1L);
-//        when(mockKundRepo.findById(1L)).thenReturn(Optional.of(mockKund));
-//
-//        Rum mockRum = new Rum();
-//        mockRum.setId(1L);
-//        when(mockRumRepo.findById(1L)).thenReturn(Optional.of(mockRum));
-//
-//
-//        Bokning expectedBokning = new Bokning();
-//        expectedBokning.setId(1L);
-//        expectedBokning.setNätter(3);
-//        expectedBokning.setStartSlutDatum("240101-240104");
-//        expectedBokning.setKund(mockKund);
-//        expectedBokning.setRum(mockRum);
-//        expectedBokning.setAvbokad(false);
-//
-//
-//        when(mockBokningRepo.save(any(Bokning.class))).thenReturn(expectedBokning);
-//
-//
-//        BokningDTO result = bokningService.addBokningFromDTO(bokningDTO);
-//
-//
-//        assertNotNull(result);
-//        assertEquals(expectedBokning.getId(), result.getId());
-//        assertEquals(expectedBokning.getNätter(), result.getNätter());
-//        assertEquals(expectedBokning.getStartSlutDatum(), result.getStartSlutDatum());
-//        assertEquals(expectedBokning.getKund().getId(), result.getKundId());
-//        assertEquals(expectedBokning.getRum().getId(), result.getRumId());
-//        assertEquals(expectedBokning.isAvbokad(), result.isAvbokad());
-//
-//        verify(mockBokningRepo, times(1)).save(any(Bokning.class));
-//    }
+    @Test
+    public void testAddBokningFromDTO() {
+        BokningDTO bokningDTO = new BokningDTO();
+        bokningDTO.setId(1L);
+        bokningDTO.setNätter(3);
+        bokningDTO.setStartDatum(LocalDate.parse("2024-01-01"));
+        bokningDTO.setSlutDatum(LocalDate.parse("2024-01-04"));
+        bokningDTO.setKundId(1L);
+        bokningDTO.setRumId(1L);
+        bokningDTO.setAvbokad(false);
+
+        Kund mockKund = new Kund();
+        mockKund.setId(1L);
+        when(mockKundRepo.findById(1L)).thenReturn(Optional.of(mockKund));
+
+        Rum mockRum = new Rum();
+        mockRum.setId(1L);
+        mockRum.setRumTyp("Single");
+        when(mockRumRepo.findById(1L)).thenReturn(Optional.of(mockRum));
+
+        double roomPrice = 100.0;
+        double discountedPrice = 90.0;
+        when(mockDiscountService.discountBokning(any(Kund.class), anyInt(), any(LocalDate.class), any(LocalDate.class), anyDouble()))
+                .thenReturn(discountedPrice);
+
+        Bokning expectedBokning = new Bokning();
+        expectedBokning.setId(1L);
+        expectedBokning.setNätter(3);
+        expectedBokning.setStartDatum(LocalDate.parse("2024-01-01"));
+        expectedBokning.setSlutDatum(LocalDate.parse("2024-01-04"));
+        expectedBokning.setKund(mockKund);
+        expectedBokning.setRum(mockRum);
+        expectedBokning.setAvbokad(false);
+
+        when(mockBokningRepo.save(any(Bokning.class))).thenReturn(expectedBokning);
+
+        BokningDTO result = bokningService.addBokningFromDTO(bokningDTO);
+
+        assertNotNull(result);
+        assertEquals(expectedBokning.getId(), result.getId());
+        assertEquals(expectedBokning.getNätter(), result.getNätter());
+        assertEquals(bokningDTO.getStartDatum(), result.getStartDatum());
+        assertEquals(bokningDTO.getSlutDatum(), result.getSlutDatum());
+        assertEquals(expectedBokning.getKund().getId(), result.getKundId());
+        assertEquals(expectedBokning.getRum().getId(), result.getRumId());
+        assertEquals(expectedBokning.isAvbokad(), result.isAvbokad());
+        verify(mockBokningRepo, times(1)).save(any(Bokning.class));
+        verify(mockDiscountService, times(1)).discountBokning(any(Kund.class), anyInt(), any(LocalDate.class), any(LocalDate.class), anyDouble());
+    }
 
 
 }
--- src/test/java/com/example/pensionatdb/Tests/RumServiceTest.java
+++ src/test/java/com/example/pensionatdb/Tests/RumServiceTest.java
@@ -22,33 +22,19 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-@SpringBootTest
 public class RumServiceTest {
 
 
     @Mock
-    private rumRepo rr;
-    @Mock
     private BokningService br;
 
     @InjectMocks
-    private RumService service = new RumService(rr, br);
+    private RumService service;
 
-
-
-    Kund kund = new Kund(1L, "Anders", "solgatan2", "hejhej");
-
-    long id = 1L;
-
-    String rumTyp = "enkelRum";
-
-    int extraSäng = 1;
-
-//    Rum rum = new Rum(id, rumTyp, extraSäng);
-
-
-//    Bokning bokning = new Bokning(1L, 3, "240510-240515", kund, rum, false);
-
+    private final long id = 1L;
+    private final String rumTyp = "enkelRum";
+    private final int extraSäng = 1;
+    private final Rum rum = new Rum(id, rumTyp, 100, extraSäng);
 
     RumDTO rumDTO = RumDTO.builder()
             .id(id)
@@ -57,39 +43,34 @@ public class RumServiceTest {
             .build();
 
 
-//    @Test
-//    void testIsRoomAvailableWhenRoomIsNotAvailable(){
-//        LocalDate startDate = LocalDate.of(2024, 5, 10);
-//        LocalDate endDate = LocalDate.of(2024, 5, 15);
-//        RumService service1 = new RumService(rr, br);
-//
-//        when(br.isRoomAvailable(rum, "240510-240515")).thenReturn(false);
-//        boolean isAvailable = service1.isRoomAvailable(rum, startDate, endDate);
-//
-//
-//        assertFalse(isAvailable);
-//    }
+    @Test
+    void testIsRoomAvailableWhenRoomIsNotAvailable() {
+        LocalDate startDate = LocalDate.of(2024, 5, 10);
+        LocalDate endDate = LocalDate.of(2024, 5, 15);
 
-//    @Test
-//    void testIsRoomAvailableWhenRoomIsAvailable(){
-//        LocalDate startDate = LocalDate.of(2024, 5, 20);
-//        LocalDate endDate = LocalDate.of(2024, 5, 25);
-//        RumService service2 = new RumService(rr, br);
-//
-//        when(br.isRoomAvailable(rum, "240520-240525")).thenReturn(true);
-//        boolean isAvailable = service2.isRoomAvailable(rum, startDate, endDate);
-//
-//
-//        assertTrue(isAvailable);
-//    }
+        when(br.isRoomAvailable(rum, startDate, endDate)).thenReturn(false);
+        boolean isAvailable = service.isRoomAvailable(rum, startDate, endDate);
 
+        assertFalse(isAvailable);
+    }
 
-//    @Test
-//    void rumDTO(){
-//
-//        RumDTO actual = service.convertToRumDTO(rum);
-//        assertEquals(actual.getId(), rumDTO.getId());
-//        assertEquals(actual.getRumTyp(), rumDTO.getRumTyp());
-//        assertEquals(actual.getExtraSäng(), rumDTO.getExtraSäng());
-//    }
+    @Test
+    void testIsRoomAvailableWhenRoomIsAvailable() {
+        LocalDate startDate = LocalDate.of(2024, 5, 20);
+        LocalDate endDate = LocalDate.of(2024, 5, 25);
+
+        when(br.isRoomAvailable(rum, startDate, endDate)).thenReturn(true);
+        boolean isAvailable = service.isRoomAvailable(rum, startDate, endDate);
+
+        assertTrue(isAvailable);
+    }
+
+    @Test
+    void rumDTO(){
+
+        RumDTO actual = service.convertToRumDTO(rum);
+        assertEquals(actual.getId(), rumDTO.getId());
+        assertEquals(actual.getRumTyp(), rumDTO.getRumTyp());
+        assertEquals(actual.getExtraSäng(), rumDTO.getExtraSäng());
+    }
 }
