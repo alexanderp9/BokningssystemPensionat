8c55cd326c45afd2cc5bba012b1fcd7ba14255ed
Ali Abdullah <aliila-3@hotmail.com>
2024-05-29 17:33:10 +0200

    PropertiesApp

--- src/main/java/com/example/pensionatdb/PensionatDbApplication.java
+++ src/main/java/com/example/pensionatdb/PensionatDbApplication.java
@@ -3,28 +3,35 @@ package com.example.pensionatdb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.boot.WebApplicationType;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class PensionatDbApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(PensionatDbApplication.class, args);
-        EventConsumer eventConsumer = context.getBean(EventConsumer.class);
-        try {
-            eventConsumer.consumeEvents();
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (args.length == 0) {
+            SpringApplication.run(PensionatDbApplication.class, args);
+        } else if (Objects.equals(args[0], "CustomerConsoleApplication")) {
+            SpringApplication application = new SpringApplication(CustomerConsoleApplication.class);
+            application.setWebApplicationType(WebApplicationType.NONE);
+            application.run(args);
+        } else if (Objects.equals(args[0], "ShipperConsoleApplication")) {
+            SpringApplication application = new SpringApplication(ShipperConsoleApplication.class);
+            application.setWebApplicationType(WebApplicationType.NONE);
+            application.run(args);
+        } else {
+            ConfigurableApplicationContext context = SpringApplication.run(PensionatDbApplication.class, args);
+            EventConsumer eventConsumer = context.getBean(EventConsumer.class);
+            try {
+                eventConsumer.consumeEvents();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
 
 
 
-//if(args.length == 0) {
-//        SpringApplication.run(PensionatDbApplication.class, args);
-//        }else if(Objects.equals(args[0], "CustomerConsoleApplication")){
-//SpringApplication application = new SpringApplication(CustomerConsoleApplication.class);
-//            application.setWebApplicationType(WebApplicationType.NONE);
-//            application.run(args);
-//
-//        }
